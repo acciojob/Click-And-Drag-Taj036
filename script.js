921432b89e1bd63be70cb321a19d125d007c325e
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-20 22:55:32 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Your code here.
+  let isDragging = false;
+  let startPosition = 0;
+  let deltaX = 0;
+
+  const items = document.querySelectorAll('.item');
+
+  items.forEach(item => {
+    item.addEventListener('mousedown', (e) => {
+      isDragging = true;
+      startPosition = e.clientX;
+    });
+  });
+
+  document.addEventListener('mouseup', () => {
+    isDragging = false;
+  });
+
+  document.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+
+    deltaX = e.clientX - startPosition;
+
+    items.forEach(item => {
+      item.style.transform = `translateX(${deltaX}px)`;
+    });
+  });
